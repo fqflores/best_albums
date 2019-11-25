77ca2f64ea0961ab9af63616469650db68aa297c
Raghu Betina <raghu@firstdraft.com>
2019-11-25 19:41:49 +0000

    Generate ratings resource with draft:resource

---
+++ db/migrate/20191125194149_create_ratings.rb
@@ -0,0 +1,11 @@
+class CreateRatings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ratings do |t|
+      t.integer :album_id
+      t.integer :user_id
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
