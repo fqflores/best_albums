f815905bf97d0711595f7a01a5f784382c0c9a55
Raghu Betina <raghu@firstdraft.com>
2019-11-25 19:43:04 +0000

    Whitelist columns in Rating for ActiveAdmin

--- app/admin/ratings.rb
+++ app/admin/ratings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Rating do
+
+ permit_params :album_id, :user_id, :rating
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
