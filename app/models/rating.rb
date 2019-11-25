4d7b7d378898248b75622e180a8210296a4dccf7
Raghu Betina <raghu@firstdraft.com>
2019-11-25 19:42:11 +0000

    Direct association between users and ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,8 @@
 class Rating < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :album
 
   # Indirect associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :class_name => "Rating",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
